d831c8fbc4ad4f90f37ff1ddfde613e0519985b4
Stern Gerlach <std.experimental.optional@gmail.com>
2021-06-08 22:31:43 +0900

    Do not inherit from Grid in GridConstant class

--- src/my_lidar_graph_slam/grid_map_new/grid_constant.cpp
+++ src/my_lidar_graph_slam/grid_map_new/grid_constant.cpp
@@ -24,10 +24,19 @@ const std::vector<double> GridConstant::ValueToOddsLookup =
 
 /* Copy constructor */
 GridConstant::GridConstant(const GridConstant& other) :
-    BaseType(other),
+    mLog2Size(other.mLog2Size),
+    mSize(other.mSize),
     mValues(nullptr)
 {
-    *this = other;
+    if (!other.IsAllocated())
+        return;
+
+    /* Allocate the storage for the internal values */
+    this->Allocate();
+
+    /* Copy the grid values */
+    const int numOfValues = 1 << (other.mLog2Size << 1);
+    std::copy_n(other.mValues.get(), numOfValues, this->mValues.get());
 }
 
 /* Copy assignment operator */
@@ -58,7 +67,8 @@ GridConstant& GridConstant::operator=(const GridConstant& other)
 
 /* Move constructor */
 GridConstant::GridConstant(GridConstant&& other) noexcept :
-    BaseType(std::move(other)),
+    mLog2Size(other.mLog2Size),
+    mSize(other.mSize),
     mValues(std::move(other.mValues))
 {
 }
@@ -69,10 +79,42 @@ GridConstant& GridConstant::operator=(GridConstant&& other) noexcept
     if (this == &other)
         return *this;
 
-    BaseType::operator=(std::move(other));
+    this->mLog2Size = other.mLog2Size;
+    this->mSize = other.mSize;
     this->mValues = std::move(other.mValues);
 
     return *this;
+}
+
+/* Initialize with the grid size */
+void GridConstant::Initialize(const int log2Size)
+{
+    /* Make sure that the specified size is positive */
+    DebugAssert(log2Size >= 0);
+
+    /* Set the size of this grid */
+    this->mLog2Size = log2Size;
+    this->mSize = 1 << log2Size;
+    /* Allocate the storage for the internal values */
+    this->Allocate();
+    /* Reset the internal values with unknown */
+    this->ResetValues();
+}
+
+/* Reset to the initial state */
+void GridConstant::Reset()
+{
+    /* Reset the size of this grid */
+    this->mLog2Size = 0;
+    this->mSize = 0;
+    /* Release the storage for the internal values */
+    this->mValues.reset(nullptr);
+}
+
+/* Reset the internal values to unknown */
+void GridConstant::ResetValues()
+{
+    this->FillValue(UnknownValue);
 }
 
 /* Allocate the storage for the internal values */
@@ -82,18 +124,6 @@ void GridConstant::Allocate()
     const int numOfValues = 1 << (this->mLog2Size << 1);
     this->mValues.reset(new std::uint16_t[numOfValues]);
     Assert(this->mValues != nullptr);
-}
-
-/* Release the storage for the internal values */
-void GridConstant::Release()
-{
-    this->mValues.reset(nullptr);
-}
-
-/* Reset the internal values to unknown */
-void GridConstant::ResetValues()
-{
-    this->FillValue(UnknownValue);
 }
 
 /* Copy the internal values to the given buffer */
