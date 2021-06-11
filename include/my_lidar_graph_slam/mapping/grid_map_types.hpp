b88a7782a67d2119d3bfce9924403afd790c388a
Stern Gerlach <std.experimental.optional@gmail.com>
2021-06-11 17:26:46 +0900

    Update the type definitions for the grid maps

--- include/my_lidar_graph_slam/mapping/grid_map_types.hpp
+++ include/my_lidar_graph_slam/mapping/grid_map_types.hpp
@@ -4,9 +4,9 @@
 #ifndef MY_LIDAR_GRAPH_SLAM_MAPPING_GRID_MAP_TYPES_HPP
 #define MY_LIDAR_GRAPH_SLAM_MAPPING_GRID_MAP_TYPES_HPP
 
-#include "my_lidar_graph_slam/grid_map/discrete_grid_cell.hpp"
-#include "my_lidar_graph_slam/grid_map/const_grid_cell.hpp"
-#include "my_lidar_graph_slam/grid_map/grid_map.hpp"
+#include "my_lidar_graph_slam/grid_map_new/grid_binary_bayes.hpp"
+#include "my_lidar_graph_slam/grid_map_new/grid_constant.hpp"
+#include "my_lidar_graph_slam/grid_map_new/grid_map.hpp"
 
 #include <type_traits>
 
@@ -15,20 +15,21 @@ namespace Mapping {
 
 /* Make sure that grid maps and precomputed grid maps store and represent
  * occupancy probability values using the same data type */
-static_assert(std::is_same<DiscreteGridCell::ValueType,
-                           ConstGridCell::ValueType>::value,
-              "DiscreteGridCell and ConstGridCell should represent "
+static_assert(std::is_same<GridMapNew::GridBinaryBayes::ProbabilityType,
+                           GridMapNew::GridConstant::ProbabilityType>::value,
+              "GridBinaryBayes and GridConstant should represent "
               "occupancy probability values using the same data type");
-static_assert(std::is_same<DiscreteGridCell::StorageType,
-                           ConstGridCell::StorageType>::value,
-              "DiscreteGridCell and ConstGridCell should store "
+static_assert(std::is_same<GridMapNew::GridBinaryBayes::ValueType,
+                           GridMapNew::GridConstant::ValueType>::value,
+              "GridBinaryBayes and GridConstant should store "
               "occupancy probability values using the same data type");
 
 /* Type definitions */
-using GridMapType = GridMap<DiscreteGridCell>;
-using ConstMapType = GridMap<ConstGridCell>;
-using GridMapInterfaceType = GridMapBase<DiscreteGridCell::ValueType,
-                                         DiscreteGridCell::StorageType>;
+using GridMap = GridMapNew::GridMap<GridMapNew::GridBinaryBayes>;
+using ConstMap = GridMapNew::GridMap<GridMapNew::GridConstant>;
+using GridMapInterface = GridMapNew::GridMapInterface<
+    GridMapNew::GridBinaryBayes::ProbabilityType,
+    GridMapNew::GridBinaryBayes::ValueType>;
 
 } /* namespace Mapping */
 } /* namespace MyLidarGraphSlam */
